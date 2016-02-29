a7fa36a88f60b32a390e6cc4ed7f770d56c0eeff
Devian <info@devian.tk>
2016-02-29 23:46:04 +0100

    Step 3: Decouple data and UI with Spacebars

---
+++ client/canvas.js
@@ -0,0 +1,12 @@
+Template.body.helpers({
+	food: [
+		{x: -100, y:-30},
+		{x: 30, y: 130},
+		{x: 87, y: 100}
+	],
+
+	otherPlayers: [
+		{x: 9, y: -150, radius: 12},
+		{x: 130, y: 30, radius: 20}
+	]
+})

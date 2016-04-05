619e6327590fce4c002b207a7bcad52fc9e537c2
Kymer <kymer.dev@icloud.com>
2016-04-05 17:02:32 +0200

    Step 4: Move within the world

--- client/canvas.js
+++ client/canvas.js
@@ -1,4 +1,6 @@
 Template.body.helpers({
+	me: {x: 10, y: 0, radius: 10},
+
 	food: [
 		{x: -100, y:-30},
 		{x: 30, y: 130},
@@ -8,5 +10,7 @@ Template.body.helpers({
 	otherPlayers: [
 		{x: 9, y: -150, radius: 12},
 		{x: 130, y: 30, radius: 20}
-	]
+	],
+
+	invert(number) {return -number}
 })

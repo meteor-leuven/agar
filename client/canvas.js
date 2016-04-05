ca2f99b7231adcef1e9885c527665b0494c4e2d9
Kymer <kymer.dev@icloud.com>
2016-04-05 17:02:53 +0200

    Step 5.2: Use food from the database

--- client/canvas.js
+++ client/canvas.js
@@ -3,11 +3,9 @@ import { Template } from 'meteor/templating'
 Template.body.helpers({
 	me: {x: 10, y: 0, radius: 10},
 
-	food: [
-		{x: -100, y:-30},
-		{x: 30, y: 130},
-		{x: 87, y: 100}
-	],
+	food: function() {
+        return Food.find()
+    },
 
 	otherPlayers: [
 		{x: 9, y: -150, radius: 12},

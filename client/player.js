a1255b9239cce3fcde51380f0e1ba0879a3437f3
Damiaan Dufaux <info@devian.tk>
2016-04-06 01:36:45 +0200

    Step 6.2: constrain player within bounds
    restrict the player to only move within the area between (-500,-500) and (500,500)

--- client/player.js
+++ client/player.js
@@ -11,11 +11,22 @@ Tracker.autorun(function() {
 // Movement
 var myVelocity = 1.5
 export const direction = [0,0]
+const myPosition = [0, 0]
+
+const clamp = (direction, position) => Math.abs(position)>500 && Math.sign(position) == Math.sign(direction) ? 0 : direction
 
 window.requestAnimationFrame(function updateMyPosition() {
 	Players.update(myPlayerId.get(), {$inc: {
-		x: direction[0]*myVelocity,
-		y: direction[1]*myVelocity
+		x: clamp(direction[0], myPosition[0])*myVelocity,
+		y: clamp(direction[1], myPosition[1])*myVelocity
 	}})
 	window.requestAnimationFrame(updateMyPosition)
+})
+
+Tracker.autorun(function() {
+	const me = Players.findOne(myPlayerId.get())
+	if (me != undefined) {
+		myPosition[0] = me.x
+		myPosition[1] = me.y
+	}
 })

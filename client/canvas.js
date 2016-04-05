7e64826013ff48de530f880375164096538ec84e
Damiaan Dufaux <info@devian.tk>
2016-04-06 01:06:41 +0200

    Step 6: Move the player around

--- client/canvas.js
+++ client/canvas.js
@@ -1,4 +1,4 @@
-import { myPlayerId } from './player'
+import { myPlayerId, direction } from './player'
 
 Template.body.helpers({
 	me() {
@@ -18,4 +18,17 @@ Template.body.helpers({
 	},
 
 	invert(number) {return -number}
+})
+
+Template.body.events({
+	'mousemove svg'(event) {
+		// Direction is a vector from the center of the window to the position of the mouse
+		direction[0] = event.clientX - event.currentTarget.offsetWidth /2
+		direction[1] = event.clientY - event.currentTarget.offsetHeight/2
+
+		// We calculate the length of the vector to normalize it
+		const length = Math.hypot(direction[0], direction[1])
+		direction[0] /= length
+		direction[1] /= length
+	}
 })
--- client/player.js
+++ client/player.js
@@ -7,3 +7,15 @@ Tracker.autorun(function() {
 			(error, result) => myPlayerId.set(result)
 		)
 })
+
+// Movement
+var myVelocity = 1.5
+export const direction = [0,0]
+
+window.requestAnimationFrame(function updateMyPosition() {
+	Players.update(myPlayerId.get(), {$inc: {
+		x: direction[0]*myVelocity,
+		y: direction[1]*myVelocity
+	}})
+	window.requestAnimationFrame(updateMyPosition)
+})

56bb988e21e801bd952301585ed68dc53b144591
Damiaan Dufaux <info@devian.tk>
2016-04-06 02:26:41 +0200

    Step 7.2: Add a game over field to Players

--- server/playerBinding.js
+++ server/playerBinding.js
@@ -3,6 +3,7 @@ import { randomLocation } from '/lib/collections';
 Meteor.onConnection(function(connection) {
 	const newPlayer = randomLocation()
 	newPlayer.points = 0;
+	newPlayer.gameOver = false
 
 	const playerId = Players.insert(newPlayer)
 	connection.playerId = playerId

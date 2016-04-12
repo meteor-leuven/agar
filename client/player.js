48b5572ea4b7701a8113dc49364de4cea5c21f03
Damiaan Dufaux <info@devian.tk>
2016-04-12 13:12:44 +0200

    Step 6.1: Add a game loop

--- client/player.js
+++ client/player.js
@@ -7,3 +7,15 @@ Tracker.autorun(function() {
 			(error, result) => myPlayerId.set(result)
 		)
 })
+
+// Movement
+var myVelocity = 1.5
+export const direction = {x: 0, y: 0}
+
+window.requestAnimationFrame(function updateMyPosition() {
+	Players.update(myPlayerId.get(), {$inc: {
+		x: direction.x*myVelocity,
+		y: direction.y*myVelocity
+	}})
+	window.requestAnimationFrame(updateMyPosition)
+})

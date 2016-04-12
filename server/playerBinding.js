d15fc4850e3f4825e801d4ca05de0f179c510b4d
Damiaan Dufaux <info@devian.tk>
2016-04-12 13:12:21 +0200

    Step 5.5: Create a player for each websocket connection

---
+++ server/playerBinding.js
@@ -0,0 +1,19 @@
+import { randomLocation } from '/lib/collections';
+
+Meteor.onConnection(function(connection) {
+	const newPlayer = randomLocation()
+	newPlayer.points = 0;
+
+	const playerId = Players.insert(newPlayer)
+	connection.playerId = playerId
+
+	connection.onClose(function() {
+		Players.remove(playerId)
+	})
+})
+
+Meteor.methods({
+	getMyPlayerId() { return this.connection.playerId }
+})
+
+Players.remove({})
